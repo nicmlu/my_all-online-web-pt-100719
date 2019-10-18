3aa1882948c93588a3d5ce1325bd104df188e1d0
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-18 04:08:43 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   idx = 0
-  while idx <= collection.length do |item| 
-    yield(item[i])
+  while idx < collection.length  
+    yield(collection[i])
+    
+    idx += 1 
   end
 end
